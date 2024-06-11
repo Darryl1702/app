209fc8d76998a718ac589cbffade850697cd703e
Darryl Llanura <darryl.llanura@gmail.com>
2024-06-11 10:36:16 +0800

    configure firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:saktong_musik/app/app.dart';
 import 'package:saktong_musik/bootstrap.dart';
+import 'package:saktong_musik/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await bootstrap(() => const App());
 }
